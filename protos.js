82d1de42b0f3f14f6079e32f67ac81a23970899a
Chris <chris@cp-graphics.com>
2015-02-14 10:04:05 -0800

    initial

---
+++ protos.js
@@ -0,0 +1,64 @@
+var Protos = (function() {
+    var uidCounter = 0;
+
+    // deep copy an object
+    function clone(src) {
+        // check for arrays too!
+        var obj = (src.hasOwnProperty('length')) ? [] : {},
+            prop;
+
+        for (prop in src) {
+            if (typeof src[prop] === 'object' && src[prop] !== null) {
+                obj[prop] = clone(src[prop]);
+            } else {
+                obj[prop] = src[prop];
+            }
+        }
+        return obj;
+    }
+
+    var Protos = function(members, alpha) {
+        var prop;
+        var Beta = function(options, extending) {
+            var prop;
+
+            for(prop in this) {
+                if (typeof this[prop] === 'object' && this[prop] !== null) {
+                    this[prop] = clone(this[prop]);
+                }
+            }
+
+            for(prop in options) {
+                if (this[prop] !== undefined) {
+                    this[prop] = options[prop];
+                }
+            }
+
+            this.uid = uidCounter++;
+
+            if (this.init && !extending) {
+                this.init(options);
+            }
+        };
+
+        for(prop in members) {
+            if (typeof members[prop] === 'object' && members[prop] !== null) {
+                alpha[prop] = clone(members[prop]);
+            } else {
+                alpha[prop] = members[prop];
+            }
+        }
+
+        for(prop in alpha) {
+            Beta.prototype[prop] = alpha[prop];
+        }
+
+        Beta.extend = function(members) {
+            return Protos(members, new this(null, true));
+        };
+
+        return Beta;
+    };
+
+    return Protos();
+}());
