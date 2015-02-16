6832b3f275fb7445db2e57a0645f4b02b8f7a600
Chris <chris@cp-graphics.com>
2015-02-15 16:36:16 -0800

    re-add preloader

---
+++ js/src/preloader.js
@@ -0,0 +1,91 @@
+SW.Preloader = Protos.extend({
+    assets: null,
+
+    total: 0,
+
+    loaded: 0,
+
+    /**
+     * @params {object} options
+     * @params {array}  options.assets - array of url paths
+     */
+    init : function(options) {
+        var prop;
+
+        this.assets = options.assets;
+
+        for(prop in this.assets) {
+            this.total += 1;
+        }
+
+        for (prop in this.assets) {
+            if (this.assets[prop].indexOf('.png') > 0 || this.assets[prop].indexOf('.jpg') > 0) {
+                var img = new Image();
+                img.src = this.assets[prop];
+
+                radio.tuneIn(img, 'load',  this.loadHandler, this);
+                radio.tuneIn(img, 'error', this.error, this);
+            } else if (this.assets[prop].indexOf('.mp3') > 0 || this.assets[prop].indexOf('.wav') > 0 || this.assets[prop].indexOf('.ogg') > 0) {
+                var audio = new Audio();
+                audio.src = this.assets[prop];
+
+                radio.tuneIn(audio, 'canplaythrough', this.loadHandler, this);
+                radio.tuneIn(audio, 'error', this.error, this);
+            }
+        }
+    },
+
+    tuneOutCurrent: function(el) {
+        var type = el.tagName.toLowerCase();
+        var name;
+
+        if (type == 'img') {
+            radio.tuneOut(el, 'load',  this.loadHandler);
+            radio.tuneOut(el, 'error', this.error);
+
+            if (this.MediaManager) {
+                for(name in this.assets) {
+                    if (this.getFileName(this.assets[name]) === this.getFileName(el.src)) {
+                        this.MediaManager.addImage(name, el);
+                    }
+                }
+            }
+        } else if (type == 'audio') {
+            radio.tuneOut(el, 'canplaythrough', this.loadHandler);
+            radio.tuneOut(el, 'error', this.error);
+
+            if (this.MediaManager) {
+                for(name in this.assets) {
+                    if (this.getFileName(this.assets[name]) === this.getFileName(el.src)) {
+                        this.MediaManager.addSound(name, el);
+                    }
+                }
+            }
+        }
+    },
+
+    loadHandler: function(e) {
+        this.tuneOutCurrent(e.currentTarget);
+
+        this.loaded += 1;
+
+        if (this.loaded === this.total) {
+            radio.broadcast('preloadcomplete');
+        } else {
+            radio.broadcast('preloadupdate', {
+                detail: {
+                    loaded: this.loaded,
+                    total : this.total
+                }
+            });
+        }
+    },
+
+    error: function(e) {
+        console.log(e.status);
+    },
+
+    getFileName: function(path) {
+        return path.substring(path.lastIndexOf('/') + 1, path.length + 1);
+    }
+});
