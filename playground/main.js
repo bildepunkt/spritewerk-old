e50c4f365b87fb3f889d8e6e3cdb861188d9af77
Chris Peters <c.concat.p@gmail.com>
2016-02-14 20:02:14 -0800

    xform hierarchies with Group!!! Huzzah!

--- playground/main.js
+++ playground/main.js
@@ -1,12 +1,10 @@
 import Camera from './src/Camera';
 import Draw from './src/Draw';
 import Input from './src/Input';
-import Mobile from './src/Mobile';
 import Stage from './src/Stage';
 import Rectangle from './src/Rectangle';
+import Group from './src/Group';
 import Ticker from './src/Ticker';
-
-Mobile.addMetaTags();
 
 let camera = new Camera();
 let stage = new Stage(800, 600, {
@@ -16,14 +14,16 @@ let stage = new Stage(800, 600, {
 let draw = new Draw(stage.getCanvas(), camera);
 let input = new Input(stage.getCanvas());
 let ticker = new Ticker();
+
+let groupA = new Group(32).setScaleX(2).setOpacity(0.5);
+let groupB = new Group(0, 32);
 let rect = new Rectangle();
+
+groupB.addItem(rect, 'rect');
+groupA.addItem(groupB, 'grp');
 
 ticker.onTick = function (factor) {
     draw.clear('#DDD');
-    console.log(factor);
 
-    let speed = 800 * factor;
-    rect.setX(rect.getX() + speed);
-
-    draw.render(rect);
+    draw.render(groupA);
 }
