6da2e521b44a96ae6b458ef530bbfbd68d489787
Chris Peters <c.concat.p@gmail.com>
2016-04-12 19:34:10 -0700

    add tests form Animation

--- src/bitmap/Animation.js
+++ src/bitmap/Animation.js
@@ -4,37 +4,43 @@
  * @desc     Describes the src positions and timing of spritesheet a animation
  * @author   Chris Peters
  *
- * @param    {Array}   srcXSequence A list of srcX positions
- * @param    {Array}   srcYSequence A list of srcY positions
- * @param    {Integer} [step=2]     The amount of frames between each frame step. This is based on
- *                                  requestAnimationFrame's 60fps rate. eg: 2 would animate @ 30fps, 3 @ 15fps etc.
- * @param    {loop}    [loop=false] If true, will start the sequence at the initial position when complete and run
- *                                  until stopped
+ * @param    {Array|Integer} srcXSequence A list of (or singular) srcX position(s)
+ * @param    {Array|Integer} srcYSequence A list of (or singular) srcY position(s)
+ * @param    {Integer}       [step=2]     The amount of frames between each frame step. This is based on
+ *                                        requestAnimationFrame's 60fps rate. eg: 2 would animate @ 30fps, 3 @ 15fps
+ *                                        etc.
+ * @param    {loop}          [loop=false] If true, will start the sequence at the initial position when complete and run
+ *                                        until stopped
  */
 export default class Animation {
     constructor(srcXSequence, srcYSequence, step = 2, loop = false) {
         this._srcXSequence = srcXSequence;
         this._srcYSequence = srcYSequence;
+        // at least one sequence must be array
         this._sequenceLength = Array.isArray(this._srcXSequence) ?
             this._srcXSequence.length :
             this._srcYSequence.length;
         this._step = step;
         this._frame = 0;
+        this._ticks = 0;
         this._playing = false;
         this._loop = loop;
     }
 
     /**
+     * @memberof bitmap
      * @method Animation#play
      * @param {Boolean} [loop] Whether or not to loop in this sequence instance
      */
     play(loop) {
         this._loop = loop !== undefined ? loop : this._loop;
+        this._ticks = 0;
         this._frame = 0;
         this._playing = true;
     }
 
     /**
+     * @memberof bitmap
      * @method Animation#update
      * @returns {null|Object} If animation not playing returns null else returns object containing current srcX/Y values
      */
@@ -43,27 +49,30 @@ export default class Animation {
             return null;
         }
 
-        const nextTick = (this._frame + 1) % this._step;
+        var srcCoords = {
+            srcX: Array.isArray(this._srcXSequence) ? this._srcXSequence[this._frame] : this._srcXSequence,
+            srcY: Array.isArray(this._srcYSequence) ? this._srcYSequence[this._frame] : this._srcYSequence
+        };
 
-        if (nextTick >= this._sequenceLength) {
+        this._ticks += 1;
+        this._frame += this._ticks % this._step === 0 ? 1 : 0;
+
+        if (this._frame >= this._sequenceLength) {
             if (this._loop) {
                 this._frame = 0;
+                this._ticks = 0;
             } else {
                 this.stop();
             }
-        } else {
-            this._frame = nextTick;
         }
 
-        return {
-            srcX: this._srcXSequence[this._frame],
-            srcY: this._srcYSequence[this._frame]
-        };
+        return srcCoords;
     }
 
     /**
      * Stops the current animation. Called automatically when sequence ends and loop set to false
      *
+     * @memberof bitmap
      * @method Animation#stop
      */
     stop() {
